97434546975bb47eaa7beefe26e44f56e47c499a
ua.lifesheets <ua.lifesheets@gmail.com>
2025-02-17 21:13:49 +0200

    Displays the page header by connecting the appropriate file

---
+++ bootstrap/classes/template.class.php
@@ -0,0 +1,9 @@
+<?php
+
+class template
+{
+    # Виводить шапку сторінки, підключаючи відповідний файл
+    public static function header() {
+        echo '<link rel="stylesheet" href="/bootstrap/assets/css/captcha.css">';
+    }
+
--- bootstrap/core.php
+++ bootstrap/core.php
@@ -3,5 +3,8 @@
 # Константи та псевдо змінних та функцій
 require_once(ROOT_DIR . '/bootstrap/system/redefinition.php');
 
+# Масиви для збереження значень
+require_once(ROOT_DIR . '/bootstrap/system/constants.php');
+
 # Автоматичне завантаження класів та функцій
 require_once ROOT_DIR . '/bootstrap/system/autoload.php';
