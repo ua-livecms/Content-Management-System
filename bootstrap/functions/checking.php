bea5361185a48c7907a16c69a54848732ba88a86
ua.lifesheets <ua.lifesheets@gmail.com>
2025-02-17 03:39:03 +0200

    Cookie data filtering.

--- bootstrap/functions/checking.php
+++ bootstrap/functions/checking.php
@@ -14,3 +14,17 @@ function escape_input(?string $text): string
     return $text === null ? '' : addslashes($text);
 }
 
+/**
+ * Фільтрація даних cookie.
+ * Очищає значення кукі від шкідливих символів.
+ *
+ * @param string|null $data Назва кукі, значення якої потрібно відфільтрувати.
+ * @return string Відфільтроване значення кукі.
+ */
+
+function filter_cookie(?string $data): string
+{
+    # Фільтрація вхідних даних з cookie, щоб уникнути шкідливих символів та забезпечити безпеку
+    return $data === null ? '' : filter_input(INPUT_COOKIE, $data, FILTER_SANITIZE_ENCODED);
+}
+
