a616d1ad693c5ddbc091041cd9f5d4ef672b27a6
ua.lifesheets <ua.lifesheets@gmail.com>
2025-02-17 22:25:24 +0200

    Displays the page header by connecting the appropriate file

--- bootstrap/classes/template.class.php
+++ bootstrap/classes/template.class.php
@@ -4,7 +4,18 @@ class template
 {
     # Виводить шапку сторінки, підключаючи відповідний файл
     public static function header() {
-        echo '<link rel="stylesheet" href="/bootstrap/assets/css/captcha.css">';
+        echo <<<HTML
+        <html lang="en">
+
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Content Management System</title>
+            <link rel="stylesheet" href="/bootstrap/assets/css/captcha.css">
+        </head>
+        
+        <body>
+        HTML;
     }
 
     # Генерує капчу
