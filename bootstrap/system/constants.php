52744fb928dbb9089809a9652031405f0be5486a
ua.lifesheets <ua.lifesheets@gmail.com>
2025-02-17 22:15:02 +0200

    Constant Site domain

--- bootstrap/system/constants.php
+++ bootstrap/system/constants.php
@@ -8,3 +8,5 @@
 define("TM", time());
 # Ім'я файлу, до якого виконується звернення
 define("PHP_SELF", _filter($_SERVER['PHP_SELF']));
+# Домен сайту
+define("HTTP_HOST", _filter($_SERVER['HTTP_HOST']));
