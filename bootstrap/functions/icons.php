fa343d2c02f3b337c6b99edf71eb77883b8ba329
ua.lifesheets <ua.lifesheets@gmail.com>
2025-02-18 00:44:05 +0200

    Adjusting Font-Awesome text icons

---
+++ bootstrap/functions/icons.php
@@ -0,0 +1,6 @@
+<?php
+
+# Коригування текстових іконок Font-Awesome
+function fa_icons($icons, $size = 15, $fw = null){
+    return "<i style='font-size: ".$size."px; vertical-align: middle' class='fa fa-".$icons." ".$fw."'></i>";
+}
