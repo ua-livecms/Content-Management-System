3de56e271613d53cb09041fd72e4a16a3acd0676
ua.lifesheets <ua.lifesheets@gmail.com>
2025-02-18 00:41:35 +0200

    Font Awesome 4.7.0

--- bootstrap/classes/template.class.php
+++ bootstrap/classes/template.class.php
@@ -4,18 +4,20 @@ class template
 {
     # Виводить шапку сторінки, підключаючи відповідний файл
     public static function header() {
-        echo <<<HTML
+        ?>
         <html lang="en">
 
         <head>
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <title>Content Management System</title>
+            <link rel="stylesheet" href="/bootstrap/assets/libs/font-awesome/font-awesome.css">
             <link rel="stylesheet" href="/bootstrap/assets/css/captcha.css">
+            <link rel="stylesheet" href="/bootstrap/assets/css/forms.css">
         </head>
-        
+
         <body>
-        HTML;
+        <?
     }
 
     # Отримання або відображення мітки.
