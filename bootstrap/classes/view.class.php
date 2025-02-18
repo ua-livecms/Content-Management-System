56b4ec61c547f40235b6cb8d27438b2a3c4d17fd
ua.lifesheets <ua.lifesheets@gmail.com>
2025-02-18 04:31:12 +0200

    Add title, description, keywords

--- bootstrap/classes/view.class.php
+++ bootstrap/classes/view.class.php
@@ -2,6 +2,50 @@
 
 class view
 {
+    # Статичні змінні для зберігання значень
+    private static string $title = '';
+    private static string $description = '';
+    private static string $keywords = '';
+
+    /**
+     * Встановлює заголовок сторінки, який відображається у вкладці браузера.
+     * Ця функція дозволяє змінити заголовок сторінки залежно від контексту.
+     *
+     * @param string $title Заголовок сторінки.
+     * @return void
+     */
+
+    public static function title(string $title): void
+    {
+        self::$title = $title;
+    }
+
+    /**
+     * Встановлює опис сторінки, який використовується пошуковими системами для SEO.
+     * Опис сторінки має допомогти користувачам зрозуміти зміст сторінки через пошукові результати.
+     *
+     * @param string $description Опис сторінки.
+     * @return void
+     */
+
+    public static function description(string $description): void
+    {
+        self::$description = $description;
+    }
+
+    /**
+     * Встановлює ключові слова для сторінки, що використовуються для покращення SEO.
+     * Ключові слова допомагають пошуковим системам правильно індексувати сторінку.
+     *
+     * @param string $keywords Ключові слова.
+     * @return void
+     */
+
+    public static function keywords(string $keywords): void
+    {
+        self::$keywords = $keywords;
+    }
+
     /**
      * Виводить шапку сторінки, підключаючи відповідний файл.
      *
@@ -11,14 +55,17 @@ class view
      * @return void
      */
 
-    public static function header() {
+    public static function header(): void
+    {
         ?>
         <html lang="en">
 
         <head>
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Content Management System</title>
+            <title><?php echo htmlspecialchars(self::$title); ?></title>
+            <meta name="description" content="<?php echo htmlspecialchars(self::$description); ?>">
+            <meta name="keywords" content="<?php echo htmlspecialchars(self::$keywords); ?>">
             <link rel="stylesheet" href="/bootstrap/assets/libs/font-awesome/font-awesome.css">
             <link rel="stylesheet" href="/bootstrap/assets/css/html.css">
             <link rel="stylesheet" href="/bootstrap/assets/css/captcha.css">
@@ -28,38 +75,6 @@ class view
         <body>
         <?php
     }
-
-    /**
-     * Встановлює заголовок сторінки.
-     *
-     * Ця функція дозволяє встановити заголовок для сторінки, який відображається у вкладці браузера.
-     * Можна реалізувати зміни заголовку в залежності від контексту сторінки.
-     *
-     * @return void
-     */
-
-    static function title() {}
-
-    /**
-     * Встановлює опис сторінки.
-     *
-     * Ця функція дозволяє задати мета-опис для сторінки, що використовується пошуковими системами.
-     * Опис допомагає покращити SEO сторінки.
-     *
-     * @return void
-     */
-
-    static function description() {}
-
-    /**
-     * Встановлює ключові слова для сторінки.
-     *
-     * Ця функція дозволяє встановити мета-ключові слова для сторінки, що також використовуються пошуковими системами для покращення SEO.
-     *
-     * @return void
-     */
-
-    static function keywords() {}
 
     /**
      * Виводить підвал сторінки, підключаючи відповідний файл.
